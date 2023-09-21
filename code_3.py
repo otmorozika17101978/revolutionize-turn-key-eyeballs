eff7befdbc433d7cf8839bb207eb8106f287cb45
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-21 23:34:27 +0300

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,18 @@
+
+def blood():
+    price = 'Away huge while officer imagine long left.'
+    price_length = len(price)
+    price_words = price.split()
+    price_count = len(price_words)
+    price_number = 49
+
+    for word in price_words:
+        print("Word:", word)
+
+    print("String:", price)
+    print("String Length:", price_length)
+    print("Number:", price_number)
+    print("Word Count:", price_count)
+
+if __name__ == "__main__":
+    blood()
