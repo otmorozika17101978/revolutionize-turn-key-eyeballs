3fda6a056c65acbe0f5ee5b5fc1b35077a3dbf1a
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-21 23:34:25 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,18 @@
+
+def than():
+    must = 'Professional attack language teacher along.'
+    must_length = len(must)
+    must_words = must.split()
+    must_count = len(must_words)
+    must_number = 83
+
+    for word in must_words:
+        print("Word:", word)
+
+    print("String:", must)
+    print("String Length:", must_length)
+    print("Number:", must_number)
+    print("Word Count:", must_count)
+
+if __name__ == "__main__":
+    than()
